2c7003e883d7b24699e7795d27b7618fd6a0b25a
samKenpachi011 <samuelkabelo1@gmail.com>
2023-07-19 18:39:08 +0200

    ADD: sample tests

---
+++ app/app/sample_calculations.py
@@ -0,0 +1,13 @@
+"""
+Creating two calculator functions for sample tests
+"""
+
+
+def add(x, y):
+    """Returns the sum"""
+    return x+y
+
+
+def subtract(x, y):
+    """Returns the difference"""
+    return x-y
