8bf7273dfe13946058fb0ba708a4af2f5df6f430
samKenpachi011 <samuelkabelo1@gmail.com>
2023-07-23 18:50:09 +0200

    UPDATE: retrieve and update user profile endpoints

--- app/user/urls.py
+++ app/user/urls.py
@@ -10,4 +10,5 @@ app_name = 'user'
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('token/', views.CreateAuthTokenView.as_view(), name='token'),
+    path('profile/', views.ManageProfileView.as_view(), name='profile'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,10 @@
 """
 Views for the user api
 """
-from rest_framework import generics
+from rest_framework import (
+    generics,
+    authentication,
+    permissions)
 from user.serializer import UserSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -18,3 +21,14 @@ class CreateAuthTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageProfileView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Return the authenticated user"""
+        return self.request.user
